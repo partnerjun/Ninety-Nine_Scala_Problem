d285ced23d429b4ee409088b3db7cb131ba25b17
partner_jun <partner_jun@live.co.kr>
2017-03-31 09:42:37 +0900

    immutable

--- src/main/scala-2.12/miscellaneous/Nonogram.scala
+++ src/main/scala-2.12/miscellaneous/Nonogram.scala
@@ -1,8 +1,6 @@
 package miscellaneous
 
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
 /**
   * @since 2017-03-25
@@ -14,12 +12,13 @@ object NonogramEnum extends Enumeration {
 }
 
 class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
-  type Board = ArrayBuffer[ArrayBuffer[NonogramEnum.Value]]
+  type Board = List[List[Value]]
+  type Value = NonogramEnum.Value
   val ROW_LENGTH: Int = colHints.size
   val COL_LENGTH: Int = rowHints.size
   val rowHintWithIndex: List[(List[Int], Int)] = rowHints.zipWithIndex
   val colHintWithIndex: List[(List[Int], Int)] = colHints.zipWithIndex
-  val board: Board = ArrayBuffer.fill(COL_LENGTH)(ArrayBuffer.fill(ROW_LENGTH)(NonogramEnum.U))
+  val board: Board = List.fill(COL_LENGTH)(List.fill(ROW_LENGTH)(NonogramEnum.U))
 
   /**
     * 전체 길이와 힌트를 입력받아
@@ -29,7 +28,7 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
     * @param hint   힌트
     * @return 만들어질 수 있는 모든 경우의 수
     */
-  def getAllCases(length: Int, hint: List[Int]): List[List[NonogramEnum.Value]] = {
+  def getAllCases(length: Int, hint: List[Int]): List[List[Value]] = {
 
     /**
       * 힌트를 입력받아 칠할 부분들을 구하는 메소드
@@ -38,7 +37,7 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
       * @param hint Int 형태의 힌트
       * @return 칠해진 부분들의 그룹
       */
-    def hintToGroup(hint: List[Int]): List[List[NonogramEnum.Value]] = hint match {
+    def hintToGroup(hint: List[Int]): List[List[Value]] = hint match {
       case h :: Nil => List(List.fill(h)(NonogramEnum.O))
       case h :: t =>
         (List.fill(h)(NonogramEnum.O) :+ NonogramEnum.X) :: hintToGroup(t)
@@ -66,8 +65,8 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
     * @param cases      경우의 수들
     * @return 이미 칠한 부분과 경우의 수를 이용해 구한 `무조건 칠해지는 부분`과 `칠해지지 않는 부분`
     */
-  def getNextFilledLine(nowFilledLine: List[NonogramEnum.Value],
-                        cases: List[List[NonogramEnum.Value]]): List[NonogramEnum.Value] = {
+  def getNextFilledLine(nowFilledLine: List[Value],
+                        cases: List[List[Value]]): List[Value] = {
 
     /**
       * 경우의 수들을 합쳐 공통된 부분을 구하는 함수
@@ -75,8 +74,8 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
       * @param cases 경우의 수들
       * @return 경우의 수들의 공통된 부분
       */
-    def foldCases(cases: List[List[NonogramEnum.Value]]): List[NonogramEnum.Value] =
-      cases.foldLeft(List.empty[NonogramEnum.Value]) { (total, caseLst) =>
+    def foldCases(cases: List[List[Value]]): List[Value] =
+      cases.foldLeft(List.empty[Value]) { (total, caseLst) =>
         if (total.isEmpty) caseLst
         else caseLst.zipWithIndex.map(c =>
           if (total(c._2) == c._1) c._1
@@ -93,8 +92,8 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
       * @return 완전히 포함하는 리스트인지 여부
       */
     @tailrec
-    def isContrayCase(lst: List[NonogramEnum.Value],
-                      that: List[NonogramEnum.Value]): Boolean = (lst, that) match {
+    def isContrayCase(lst: List[Value],
+                      that: List[Value]): Boolean = (lst, that) match {
       case (Nil, Nil) => false
       case (lstH :: lstT, thatH :: thatT) =>
         if (lstH == NonogramEnum.X && thatH == NonogramEnum.O) true
@@ -109,14 +108,87 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
 
 
   /**
+    *  행이 칠해진 보드를 리턴하는 메소드
+    *
+    * @param board 칠하려고 하는 보드
+    * @param col 칠하려고 하는 열의 인덱스
+    * @param valueLst 칠하려고 하는 값들
+    * @return (칠해진 보드, 칠해진 내역)
+    */
+  def updateRow(board: Board, col: Int, valueLst:List[Value]): (Board, List[Value]) = {
+    def getUpdatedLst(lst:List[Value], valueLst: List[Value]): List[Value] = (lst, valueLst) match {
+      case (l, Nil) => l
+      case (lstH::lstT, valueH::valueT) =>
+        val updatedValue = if(lstH == NonogramEnum.U) valueH else lstH
+        updatedValue :: getUpdatedLst(lstT, valueT)
+    }
+    val updatedLst = getUpdatedLst(board(col), valueLst)
+    (board.updated(col, updatedLst), updatedLst)
+  }
+
+  /**
+    *  행이 완성되었을 때 칠해진 부분만 남기는 메소드
+    *
+    * @param board 보드
+    * @param col 완성된 행의 열 인덱스
+    * @return 칠해진 부분만 남은 보드
+    */
+  def closeRow(board: Board, col: Int): Board = {
+    val lst = board(col).map(v => if(v != NonogramEnum.O) NonogramEnum.X else v)
+    board.updated(col, lst)
+  }
+
+  /**
+    * 열이 칠해진 보드를 리턴하는 메소드
+    *
+    * @param board 칠하려고 하는 보드
+    * @param row 칠하려고 하는 행의 인덱스
+    * @param valueLst 칠하려고 하는 값들
+    * @return (칠해진 보드, 칠해진 내역)
+    */
+  def updateCol(board: Board, row: Int, valueLst: List[Value]): (Board, List[Value]) = {
+    def updateColRec(board: Board, row: Int, valueLst: List[Value], col: Int): (Board, List[Value]) = valueLst match {
+      case Nil => (board, Nil)
+      case valueH :: valueT =>
+        val boardValue = board(col)(row)
+        val updatedValue = if (boardValue == NonogramEnum.U) valueH else boardValue
+        val filledBoard = board.updated(col, board(col).updated(row, updatedValue))
+        val (newBoard, updatedLst) = updateColRec(filledBoard, row, valueT, col + 1)
+        (newBoard, updatedValue :: updatedLst)
+    }
+    updateColRec(board, row, valueLst, 0)
+  }
+
+  /**
+    *  열이 완성되었을 때 칠해진 부분만 남기는 메소드
+    *
+    * @param board 보드
+    * @param col 완성된 열의 행 인덱스
+    * @return 칠해진 부분만 남은 보드
+    */
+  def closeCol(board: Board, col: Int): Board = {
+    @tailrec
+    def closeColRec(board: Board, row: Int, col: Int): Board = {
+      if(col >= COL_LENGTH) return board
+      val v = if(board(col)(row) != NonogramEnum.O) NonogramEnum.X else NonogramEnum.O
+      val nextBoard = board.updated(col, board(col).updated(row, v))
+      closeColRec(nextBoard, row, col + 1)
+    }
+    closeColRec(board, col, 0)
+  }
+
+
+
+  /**
     * 리스트가 힌트를 모두 충족한 완성된 리스트인지 체크하는 메소드
     *
     * @param lst  대상 리스트
     * @param hint 힌트
     * @return 완성된 리스트인지 여부
     */
-  private def vaildate(lst: List[NonogramEnum.Value], hint: List[Int]): Boolean =
+  private def vaildate(lst: List[Value], hint: List[Int]): Boolean =
     lst.count(_ == NonogramEnum.O) == hint.sum
+
 
   /**
     * 문제를 풀어내는 메소드
@@ -136,46 +208,30 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
                           colHint: List[(List[Int], Int)],
                           isRow: Boolean = true, vailCount: Int = 0): String = {
 
-    if (isRow) rowHint match {
-      case rh :: rt =>
-        val (hint, i) = rh
-        val result = getNextFilledLine(board(i).toList, getAllCases(ROW_LENGTH, hint))
+    // row/column 상황에 맞춰 함수 설정
+    val (updateFunction, closeFunction, length, hint) = if(isRow) (updateRow _, closeRow _, ROW_LENGTH, rowHint)
+                                                        else (updateCol _, closeCol _, COL_LENGTH, colHint)
+    hint match {
+      case hintH :: hintT =>
+        val (hint, i) = hintH
+        val line = if(isRow) board(i) else board.map(_(i))
+        val newFill = getNextFilledLine(line, getAllCases(length, hint))
 
-        // O 혹은 X로 확정된 지점을 채워넣음.
-        result.zipWithIndex.withFilter(v => board(i)(v._2) == NonogramEnum.U)
-          .foreach(v => board(i)(v._2) = v._1)
+        // 현재에 상황에 맞는 함수로 보드를 채운다.
+        val (filledBoard: Board, updatedLst: List[Value]) = updateFunction(board, i, newFill)
 
         // '완전히 풀어낸 부분' 인지 확인하고 완성된 줄의 수를 체크한다.
-        val nowVailCount = if (vaildate(board(i).toList, hint)) {
-          for (v <- 0 until ROW_LENGTH)
-            if (board(i)(v) != NonogramEnum.O) board(i)(v) = NonogramEnum.X
-          vailCount + 1
-        } else vailCount
+        val (nextBoard, nowVailCount) = if(vaildate(updatedLst, hint))
+                                          // '완전히 풀어낸 부분'이면 칠해진 부분만 남긴다
+                                          (closeFunction(filledBoard, i), vailCount + 1)
+                                        else (filledBoard, vailCount)
 
-        solve(board, rt, colHint, isRow = true, nowVailCount)
+        solve(nextBoard, hintT, hintT, isRow, nowVailCount)
       case Nil =>
-        if (vailCount == COL_LENGTH) board
-        else solve(board, rowHintWithIndex, colHintWithIndex, isRow = false)
-
-    } else colHint match {
-      case ch :: ct =>
-        val (hint, i) = ch
-        val result = getNextFilledLine(board.map(_ (i)).toList, getAllCases(COL_LENGTH, hint))
-        result.zipWithIndex.withFilter(v => board(v._2)(i) == NonogramEnum.U)
-          .foreach(v => board(v._2)(i) = v._1)
-        val nowVailCount = if (vaildate(board.map(_ (i)).toList, hint)) {
-          for (v <- 0 until COL_LENGTH)
-            if (board(v)(i) != NonogramEnum.O) board(v)(i) = NonogramEnum.X
-          vailCount + 1
-        } else vailCount
-
-        solve(board, rowHint, ct, isRow = false, nowVailCount)
-      case Nil =>
-        if (vailCount == ROW_LENGTH) board
-        else solve(board, rowHintWithIndex, colHintWithIndex)
+        if (vailCount == length) board
+        else solve(board, rowHintWithIndex, colHintWithIndex, !isRow)
     }
   }
-
 
   /**
     * 보드를 출력용 문자열로 변환하는 메소드
@@ -188,7 +244,7 @@ class Nonogram (rowHints: List[List[Int]], colHints: List[List[Int]]) {
     board.foreach { col =>
       col.foreach { point =>
         if (point == NonogramEnum.O) sb.append("| O ")
-        // else if(row == NonogramEnum.X) sb.append("| X ")
+//         else if(point == NonogramEnum.X) sb.append("| X ")
         else sb.append("|   ")
       }
       sb.append("|\n")
